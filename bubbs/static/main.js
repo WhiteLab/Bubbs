1df2b94a1cf048e2b86eab3672f71a6071b9056c
Dominic Fitzgerald <djf604@truman.edu>
2018-09-07 15:13:18 -0500

    Some url fixes

--- bubbs/static/main.js
+++ bubbs/static/main.js
@@ -1,7 +1,7 @@
 $(function() {
     $('#generate-btn').click(function() {
         var requested = $('#requested').val();
-        $.get('/bid/generate/' + requested + '/', function(data) {
+        $.get('/bubbs/bid/generate/' + requested + '/', function(data) {
             $('#generated').val(JSON.parse(data).join('\n'));
         });
     });
--- bubbs/urls.py
+++ bubbs/urls.py
@@ -5,6 +5,7 @@ from bubbs.views import Bid
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='bubbs/home.html')),
+    path('bid/', TemplateView.as_view(template_name='bubbs/bid.html'), name='bid'),
 
     path('bid/generate/', Bid.Generate.as_view()),
     path('bid/generate/<int:n>/', Bid.Generate.as_view())
